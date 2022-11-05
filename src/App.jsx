1a542fb2007c1d539de5389b138f0567c2bc4dc5
Aryan Kumar <aryankumar1504@gmail.com>
2022-11-05 23:31:02 +0530

    Created a template for react tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main className="grid place-items-center h-screen text-6xl font-semibold">
+        <h1>
+            <p className="text-center my-6 text-5xl">Sample Project</p> 
+            <p>Vite + React + Tailwind CSS</p>
+        </h1>
+    </main>
+  )
+}
+
+export default App
